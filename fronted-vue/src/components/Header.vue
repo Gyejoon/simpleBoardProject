b3960a94292275fd0ed21d384f2023f2ced653d0
jyKang2 <jy.kang@ilts.co.kr>
2019-11-20 19:44:37 +0900

    기존 구조 변경

---
+++ fronted-vue/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="scss">
+.header {
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    background-color: blue;
+}
+</style>
