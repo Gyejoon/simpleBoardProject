e4d38cda453f6100685d39f6d8c13189bea5a795
Gyejoon <00700mm@gmail.com>
2019-11-21 19:57:02 +0900

    작업분 반영

--- frontend-vue/src/components/Header.vue
+++ frontend-vue/src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="header">
-    <span>SimpleBoard</span>
+    <div class="wrapper">
+      <router-link to="/">SimpleBoard</router-link>
+    </div>
   </div>
 </template>
 
@@ -13,21 +15,27 @@ export default class Header extends Vue {}
 
 <style lang="scss">
 .header {
-  position: relative;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
+  display: block;
   width: 100%;
   height: 64px;
   max-height: 64px;
-  background-color: #3578ff;
+  background-color: #2196f3;
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
 
-  span {
-    text-align: center;
-    font-weight: 600;
-    color: white;
-    font-size: 2rem;
+  .wrapper {
+    position: relative;
+    line-height: 64px;
+    height: 100%;
+
+    a {
+      position: absolute;
+      color: #fff;
+      display: inline-block;
+      font-size: 2.1rem;
+      padding: 0;
+      white-space: nowrap;
+      text-decoration: none;
+    }
   }
 }
 </style>
